8b234c382ef4d0e5a17408dc95309ed9bcfa71cd
IvanildeSLacey <sobierai.ivanilde@gmail.com>
2022-12-12 15:30:37 +0000

    Add carousel css style and javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,24 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
 }))
+
+// Carousel slide script 
+const carouselSlide = document.querySelectorAll(".img-container img");
+
+const timeDelay = 6000;
+let currentImageCounter = 0; // setting a variable to keep track of the current image (slide)
+
+// carouselSlide[currentImageCounter].style.display = "block";
+carouselSlide[currentImageCounter].style.opacity = 1;
+
+setInterval(nextImage, timeDelay);
+
+function nextImage() {
+  // carouselSlide[currentImageCounter].style.display = "none";
+  carouselSlide[currentImageCounter].style.opacity = 0;
+
+  currentImageCounter = (currentImageCounter+1) % carouselSlide.length;
+
+  // carouselSlide[currentImageCounter].style.display = "block";
+  carouselSlide[currentImageCounter].style.opacity = 1;
+}
