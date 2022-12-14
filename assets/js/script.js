465383f2f4696b84edace18370db553db1e106f2
IvanildeSLacey <sobierai.ivanilde@gmail.com>
2022-12-14 11:41:14 +0000

    Add and style recent work image slider to main page

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,3 +52,35 @@ function nextImage() {
   // carouselSlide[currentImageCounter].style.display = "block";
   carouselSlide[currentImageCounter].style.opacity = 1;
 }
+
+//recent work image galery function
+let thumbnails = document.getElementsByClassName('thumbnail')
+
+		let activeImages = document.getElementsByClassName('active')
+
+		for (var i=0; i < thumbnails.length; i++){
+
+			thumbnails[i].addEventListener('mouseover', function(){
+				console.log(activeImages)
+				
+				if (activeImages.length > 0){
+					activeImages[0].classList.remove('active')
+				}
+				
+
+				this.classList.add('active')
+				document.getElementById('featured').src = this.src
+			})
+		}
+
+
+		let buttonRight = document.getElementById('slideRight');
+		let buttonLeft = document.getElementById('slideLeft');
+
+		buttonLeft.addEventListener('click', function(){
+			document.getElementById('slider').scrollLeft -= 180
+		})
+
+		buttonRight.addEventListener('click', function(){
+			document.getElementById('slider').scrollLeft += 180
+		})
