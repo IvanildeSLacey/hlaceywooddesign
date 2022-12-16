a724478d0890e9607ba719371edc2cc5ecb86f04
IvanildeSLacey <sobierai.ivanilde@gmail.com>
2022-12-16 18:00:09 +0000

    Structure javascript code

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,8 @@ const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
+	hamburger.classList.toggle("active");
+	navMenu.classList.toggle("active");
 })
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
@@ -56,98 +56,30 @@ function nextImage() {
 //recent work image galery function
 let thumbnails = document.getElementsByClassName('thumbnail')
 
-		let activeImages = document.getElementsByClassName('active')
+let activeImages = document.getElementsByClassName('active')
 
-		for (var i=0; i < thumbnails.length; i++){
+for (var i=0; i < thumbnails.length; i++){
 
-			thumbnails[i].addEventListener('mouseover', function(){
-				console.log(activeImages)
-				
-				if (activeImages.length > 0){
-					activeImages[0].classList.remove('active')
-				}
-				
+	thumbnails[i].addEventListener('mouseover', function(){
+		console.log(activeImages)		
+			if (activeImages.length > 0){
+				activeImages[0].classList.remove('active')
+		}	
 
-				this.classList.add('active')
-				document.getElementById('featured').src = this.src
-			})
-		}
-
-
-		let buttonRight = document.getElementById('slideRight');
-		let buttonLeft = document.getElementById('slideLeft');
-
-		buttonLeft.addEventListener('click', function(){
-			document.getElementById('slider').scrollLeft -= 180
+		this.classList.add('active')
+			document.getElementById('featured').src = this.src
 		})
+	}
+	
+	let buttonRight = document.getElementById('slideRight');
+	let buttonLeft = document.getElementById('slideLeft');
 
-		buttonRight.addEventListener('click', function(){
-			document.getElementById('slider').scrollLeft += 180
-		})
+	buttonLeft.addEventListener('click', function(){
+		document.getElementById('slider').scrollLeft -= 180
+	})
 
-// //getting all required elements
-// const gallery  = document.querySelectorAll(".image"),
-// previewBox = document.querySelector(".preview-box"),
-// previewImg = previewBox.querySelector("img"),
-// closeIcon = previewBox.querySelector(".icon"),
-// currentImg = previewBox.querySelector(".current-img"),
-// totalImg = previewBox.querySelector(".total-img"),
-// shadow = document.querySelector(".shadow");
-// window.onload = ()=>{
-//     for (let i = 0; i < gallery.length; i++) {
-//         totalImg.textContent = gallery.length; //passing total img length to totalImg variable
-//         let newIndex = i; //passing i value to newIndex variable
-//         let clickedImgIndex; //creating new variable
-        
-//         gallery[i].onclick = () =>{
-//             clickedImgIndex = i; //passing cliked image index to created variable (clickedImgIndex)
-//             function preview(){
-//                 currentImg.textContent = newIndex + 1; //passing current img index to currentImg varible with adding +1
-//                 let imageURL = gallery[newIndex].querySelector("img").src; //getting user clicked img url
-//                 previewImg.src = imageURL; //passing user clicked img url in previewImg src
-//             }
-//             preview(); //calling above function
-    
-//             const prevBtn = document.querySelector(".prev");
-//             const nextBtn = document.querySelector(".next");
-//             if(newIndex == 0){ //if index value is equal to 0 then hide prevBtn
-//                 prevBtn.style.display = "none"; 
-//             }
-//             if(newIndex >= gallery.length - 1){ //if index value is greater and equal to gallery length by -1 then hide nextBtn
-//                 nextBtn.style.display = "none"; 
-//             }
-//             prevBtn.onclick = ()=>{ 
-//                 newIndex--; //decrement index
-//                 if(newIndex == 0){
-//                     preview(); 
-//                     prevBtn.style.display = "none"; 
-//                 }else{
-//                     preview();
-//                     nextBtn.style.display = "block";
-//                 } 
-//             }
-//             nextBtn.onclick = ()=>{ 
-//                 newIndex++; //increment index
-//                 if(newIndex >= gallery.length - 1){
-//                     preview(); 
-//                     nextBtn.style.display = "none";
-//                 }else{
-//                     preview(); 
-//                     prevBtn.style.display = "block";
-//                 }
-//             }
-//             document.querySelector("body").style.overflow = "hidden";
-//             previewBox.classList.add("show"); 
-//             shadow.style.display = "block"; 
-//             closeIcon.onclick = ()=>{
-//                 newIndex = clickedImgIndex; //assigning user first clicked img index to newIndex
-//                 prevBtn.style.display = "block"; 
-//                 nextBtn.style.display = "block";
-//                 previewBox.classList.remove("show");
-//                 shadow.style.display = "none";
-//                 document.querySelector("body").style.overflow = "scroll";
-//             }
-//         }
-        
-//     } 
-// }
+	buttonRight.addEventListener('click', function(){
+		document.getElementById('slider').scrollLeft += 180
+	})
+
+
