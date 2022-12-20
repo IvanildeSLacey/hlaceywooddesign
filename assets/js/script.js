f24fb5438d6de008a192236eeded3b380ba7f17d
IvanildeSLacey <sobierai.ivanilde@gmail.com>
2022-12-20 23:01:01 +0000

    Change nav-menu for just html and css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,3 @@
-//nav hamburger menu
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-
-hamburger.addEventListener("click", () => {
-	hamburger.classList.toggle("active");
-	navMenu.classList.toggle("active");
-})
-
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
-}))
-
 //Form focus to information input area
 const inputs = document.querySelectorAll(".input");
 
