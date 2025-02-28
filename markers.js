1ab8ffc15f774f40f2a942e2051c54b225a2428d
IvanildeSLacey <sobierai.ivanilde@gmail.com>
2025-02-28 15:44:01 +0000

    test again

--- markers.js
+++ markers.js
@@ -17,13 +17,17 @@ var greenIcon = L.icon({
 
 // Function to add a dual-color marker with a link and tooltip
 function addDualColorMarker(lat, lng, popupContent, tooltipContent) {
-    var redMarker = L.marker([lat, lng], {icon: redIcon}).addTo(map);
-    var greenMarker = L.marker([lat + 0.0001, lng], {icon: greenIcon}).addTo(map); // Slight offset to show dual color
+    if (map) {
+        var redMarker = L.marker([lat, lng], {icon: redIcon}).addTo(map);
+        var greenMarker = L.marker([lat + 0.0001, lng], {icon: greenIcon}).addTo(map); // Slight offset to show dual color
 
-    redMarker.bindPopup(popupContent);
-    redMarker.bindTooltip(tooltipContent, {permanent: false, direction: 'top'});
-    greenMarker.bindPopup(popupContent);
-    greenMarker.bindTooltip(tooltipContent, {permanent: false, direction: 'top'});
+        redMarker.bindPopup(popupContent);
+        redMarker.bindTooltip(tooltipContent, {permanent: false, direction: 'top'});
+        greenMarker.bindPopup(popupContent);
+        greenMarker.bindTooltip(tooltipContent, {permanent: false, direction: 'top'});
+    } else {
+        console.error('Map instance not found.');
+    }
 }
 
 // Add markers for the given addresses with links and tooltips
@@ -32,4 +36,4 @@ addDualColorMarker(52.6730, -8.6189, '<a href="https://example.com/E41PF60" targ
 addDualColorMarker(51.939103, 0.811446, '<a href="https://example.com/Greenacres" target="_blank">Greenacres</a>', 'Greenacres, Packards Lane, Wormingford');
 addDualColorMarker(54.9382, -3.0008, '<a href="https://example.com/Andidrain" target="_blank">Andidrain Ltd</a>', 'Andidrain Ltd, Caron House, Rockcliffe Estate');
 addDualColorMarker(53.990003, -1.537033, '<a href="https://example.com/StationParade" target="_blank">80 Station Parade</a>', '80 Station Parade, Harrogate');
-addDualColorMarker(51.9014, -8.3726, '<a href="https://example.com/IslandCorporate
+addDualColorMarker(51.9014, -8.3726, '<a href="https://example.com/IslandCorporate" target="_blank">Island Corporate Park</a>', 'Island Corporate Park, Cork');
