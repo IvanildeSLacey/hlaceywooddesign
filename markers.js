e03612bfb80d149fb86167cd206c3ea32e2aad09
IvanildeSLacey <sobierai.ivanilde@gmail.com>
2025-02-28 12:02:20 +0000

    saving new code for hoover tag

--- markers.js
+++ markers.js
@@ -1,9 +1,10 @@
-// Function to add a marker with a link
-function addMarker(lat, lng, popupContent) {
+// Function to add a marker with a link and tooltip
+function addMarker(lat, lng, popupContent, tooltipContent) {
     var marker = L.marker([lat, lng]).addTo(map);
     marker.bindPopup(popupContent);
+    marker.bindTooltip(tooltipContent, {permanent: false, direction: 'top'});
 }
 
-// Add markers for the given addresses with links
-addMarker(52.6680, -8.6305, '<a href="https://example.com/E41T886" target="_blank">E41T886</a>');
-addMarker(52.6730, -8.6189, '<a href="https://example.com/E41PF60" target="_blank">E41PF60</a>');
+// Add markers for the given addresses with links and tooltips
+addMarker(52.6680, -8.6305, '<a href="https://example.com/E41T886" target="_blank">E41T886</a>', 'First Location');
+addMarker(52.6730, -8.6189, '<a href="https://example.com/E41PF60" target="_blank">E41PF60</a>', 'Second Location');
