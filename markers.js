801eb607432e16451568c68bd718d16d45b58585
IvanildeSLacey <sobierai.ivanilde@gmail.com>
2025-02-28 20:37:41 +0000

    delete marker

--- markers.js
+++ markers.js
@@ -6,7 +6,6 @@ function addMarker(lat, lng, popupContent, tooltipContent) {
 }
 
 // Add markers for the given addresses with links and tooltips
-addMarker(52.6680, -8.6305, '<a href="https://example.com/E41T886" target="_blank">E41T886</a>', 'Test');
 addMarker(52.6730, -8.6189, '<a href="https://example.com/E41PF60" target="_blank">E41PF60</a>', 'Roadvacs IRL Ltd');
 addMarker(51.939103, 0.811446, '<a href="https://example.com/Greenacres" target="_blank">Greenacres</a>', 'AA Turner Tankers');
 addMarker(54.9382, -3.0008, '<a href="https://example.com/Andidrain" target="_blank">Andidrain Ltd</a>', 'AD Andidrain Ltd');
