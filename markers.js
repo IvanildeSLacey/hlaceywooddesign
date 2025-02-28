ec1bc2bc4270951230281c8be246f5de3e243d6e
IvanildeSLacey <sobierai.ivanilde@gmail.com>
2025-02-28 16:31:55 +0000

    adding addresses

--- markers.js
+++ markers.js
@@ -12,3 +12,10 @@ addMarker(51.939103, 0.811446, '<a href="https://example.com/Greenacres" target=
 addMarker(54.9382, -3.0008, '<a href="https://example.com/Andidrain" target="_blank">Andidrain Ltd</a>', 'AD Andidrain Ltd');
 addMarker(53.990003, -1.537033, '<a href="https://example.com/StationParade" target="_blank">80 Station Parade</a>', 'Adler & Allan');
 addMarker(51.9014, -8.3726, '<a href="https://example.com/IslandCorporatePark" target="_blank">Island Corporate Park</a>', 'Ambipar Group');
+addMarker(51.9014, -8.3726, '<a href="https://example.com/basementpumps" target="_blank">basement pumps</a>', 'Basement Pumps');
+addMarker(55.5980, -4.5150, '<a href="https://example.com/BillyBowie" target="_blank">Billy Bowie Special Projects Ltd</a>', 'Billy Bowie');
+addMarker(54.4690, -1.1890, '<a href="https://example.com/RoseberryCourt" target="_blank">Roseberry Court</a>', 'Bull Tankers');
+addMarker(50.8570, 0.5400, '<a href="https://example.com/BexhillRoad" target="_blank">Bexhill Road</a>', 'Cappagh Browne');
+addMarker(52.2250, 0.8000, '<a href="https://example.com/LowerGorse" target="_blank">Lower Gorse</a>', 'Cargate Engineering');
+addMarker(52.8430, -8.9860, '<a href="https://example.com/TracklandsBusinessPark" target="_blank">Tracklands Business Park</a>', 'CES Environmental');
+addMarker(52.8430, -8.9860, '<a href="https://example.com/QuinRoadBusinessPark" target="_blank">Quin Road Business Park</a>', 'Clare Drains');
