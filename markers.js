ccebc017ea0f1a3a0209782c01238c8a5d439700
IvanildeSLacey <sobierai.ivanilde@gmail.com>
2025-02-28 16:02:20 +0000

    testing map colors

--- markers.js
+++ markers.js
@@ -15,25 +15,39 @@ var greenIcon = L.icon({
     tooltipAnchor: [16, -28]
 });
 
-// Function to add a dual-color marker with a link and tooltip
-function addDualColorMarker(lat, lng, popupContent, tooltipContent) {
+// Function to add a marker with a link and tooltip
+function addMarker(lat, lng, popupContent, tooltipContent, icon) {
     if (map) {
-        var redMarker = L.marker([lat, lng], {icon: redIcon}).addTo(map);
-        var greenMarker = L.marker([lat + 0.0001, lng], {icon: greenIcon}).addTo(map); // Slight offset to show dual color
-
-        redMarker.bindPopup(popupContent);
-        redMarker.bindTooltip(tooltipContent, {permanent: false, direction: 'top'});
-        greenMarker.bindPopup(popupContent);
-        greenMarker.bindTooltip(tooltipContent, {permanent: false, direction: 'top'});
+        var marker = L.marker([lat, lng], {icon: icon}).addTo(map);
+        marker.bindPopup(popupContent);
+        marker.bindTooltip(tooltipContent, {permanent: false, direction: 'top'});
     } else {
         console.error('Map instance not found.');
     }
 }
 
-// Add markers for the given addresses with links and tooltips
-addDualColorMarker(52.6680, -8.6305, '<a href="https://example.com/E41T886" target="_blank">E41T886</a>', 'First Location');
-addDualColorMarker(52.6730, -8.6189, '<a href="https://example.com/E41PF60" target="_blank">E41PF60</a>', 'Second Location');
-addDualColorMarker(51.939103, 0.811446, '<a href="https://example.com/Greenacres" target="_blank">Greenacres</a>', 'Greenacres, Packards Lane, Wormingford');
-addDualColorMarker(54.9382, -3.0008, '<a href="https://example.com/Andidrain" target="_blank">Andidrain Ltd</a>', 'Andidrain Ltd, Caron House, Rockcliffe Estate');
-addDualColorMarker(53.990003, -1.537033, '<a href="https://example.com/StationParade" target="_blank">80 Station Parade</a>', '80 Station Parade, Harrogate');
-addDualColorMarker(51.9014, -8.3726, '<a href="https://example.com/IslandCorporate" target="_blank">Island Corporate Park</a>', 'Island Corporate Park, Cork');
+// List of addresses with red markers
+var redAddresses = [
+    {lat: 52.6680, lng: -8.6305, popup: '<a href="https://example.com/E41T886" target="_blank">E41T886</a>', tooltip: 'First Location'},
+    {lat: 54.9382, lng: -3.0008, popup: '<a href="https://example.com/Andidrain" target="_blank">Andidrain Ltd</a>', tooltip: 'Andidrain Ltd, Caron House, Rockcliffe Estate'},
+    // Add more red addresses here
+];
+
+// List of addresses with green markers
+var greenAddresses = [
+    {lat: 52.6730, lng: -8.6189, popup: '<a href="https://example.com/E41PF60" target="_blank">E41PF60</a>', tooltip: 'Second Location'},
+    {lat: 51.939103, lng: 0.811446, popup: '<a href="https://example.com/Greenacres" target="_blank">Greenacres</a>', tooltip: 'Greenacres, Packards Lane, Wormingford'},
+    {lat: 53.990003, lng: -1.537033, popup: '<a href="https://example.com/StationParade" target="_blank">80 Station Parade</a>', tooltip: '80 Station Parade, Harrogate'},
+    {lat: 51.9014, lng: -8.3726, popup: '<a href="https://example.com/IslandCorporate" target="_blank">Island Corporate Park</a>', tooltip: 'Island Corporate Park, Cork'},
+    // Add more green addresses here
+];
+
+// Add red markers
+redAddresses.forEach(function(address) {
+    addMarker(address.lat, address.lng, address.popup, address.tooltip, redIcon);
+});
+
+// Add green markers
+greenAddresses.forEach(function(address) {
+    addMarker(address.lat, address.lng, address.popup, address.tooltip, greenIcon);
+});
