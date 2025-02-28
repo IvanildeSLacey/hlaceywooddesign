b70902353b3b06ab78a14207aa9a0deb05dcb16f
IvanildeSLacey <sobierai.ivanilde@gmail.com>
2025-02-28 15:20:38 +0000

    back in the test

--- markers.js
+++ markers.js
@@ -1,21 +1,8 @@
-// Function to add a marker with a specified color
-function addMarker(lat, lng, info, title, color) {
-    let marker = new google.maps.Marker({
-        position: {lat: lat, lng: lng},
-        map: map,
-        title: title,
-        label: { color: color, text: title }, // Add color to label
-    });
-
-    // Attach the information window to the marker
-    let infoWindow = new google.maps.InfoWindow({
-        content: info
-    });
-
-    // Add an event listener for marker click
-    marker.addListener('click', function() {
-        infoWindow.open(map, marker);
-    });
+// Function to add a marker with a link and tooltip
+function addMarker(lat, lng, popupContent, tooltipContent) {
+    var marker = L.marker([lat, lng]).addTo(map);
+    marker.bindPopup(popupContent);
+    marker.bindTooltip(tooltipContent, {permanent: false, direction: 'top'});
 }
 
 // Add markers for the given addresses with links and tooltips
@@ -25,10 +12,3 @@ addMarker(51.939103, 0.811446, '<a href="https://example.com/Greenacres" target=
 addMarker(54.9382, -3.0008, '<a href="https://example.com/Andidrain" target="_blank">Andidrain Ltd</a>', 'Andidrain Ltd, Caron House, Rockcliffe Estate');
 addMarker(53.990003, -1.537033, '<a href="https://example.com/StationParade" target="_blank">80 Station Parade</a>', '80 Station Parade, Harrogate');
 addMarker(51.9014, -8.3726, '<a href="https://example.com/IslandCorporatePark" target="_blank">Island Corporate Park</a>', 'Island Corporate Park, Wallingstown, Little Island');
-
-
-// Add green marker
-addMarker(51.9014, -8.3726, '<a href="https://example.com/IslandCorporatePark" target="_blank">Island Corporate Park</a>', 'Island Corporate Park, Wallingstown, Little Island', 'green');
-
-// Add red marker (example address)
-addMarker(51.8974, -8.4705, '<a href="https://example.com/exampleAddress" target="_blank">Example Address</a>', 'Example Address, Cork', 'red');
