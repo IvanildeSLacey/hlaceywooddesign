d6e2b60917cf5afe27893e307d496b869a5e0f82
IvanildeSLacey <sobierai.ivanilde@gmail.com>
2025-03-12 16:16:03 +0000

    amending marker

--- assets/js/mapscript.js
+++ assets/js/mapscript.js
@@ -103,6 +103,6 @@ addMarker(52.00279982846905, -2.0562475243113942, '<a href="https://example.com/
 
 // Add markers for our potential customers, red,  with links and tooltips
 addMarker(53.97336261621961, -6.720400815345416, '<a href="https://example.com/E41PF60" target="_blank">E41PF60</a>', 'JC Enviro', redIcon);
-addMarker(53.1171544607595, -6.096267701854477, '<a href="https://example.com/E41PF60" target="_blank">E41PF60</a>', 'Kelly Drain', redIcon);
+addMarker(53.11714560680234, -6.096205340493168, '<a href="https://example.com/E41PF60" target="_blank">E41PF60</a>', 'Kelly Drain', redIcon);
 // Set the map view to the bounds
 map.fitBounds(bounds);
